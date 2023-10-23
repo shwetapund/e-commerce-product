11d122ecf8ed7b12b192350e61ff1cae232a8e8a
shwetapund <pundshweta2020@gmail.com>
2023-10-23 13:21:50 -0700

    fetch data from database and display in webpage

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,20 +1,52 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import './Home.css'
 
 function Home() {
-    
-    const product = [
-        {
-            name: 
-        }
-    ]
+
+  const [product, setProduct] = useState([])
+  const loadProduct = async () => {
+    const response = await axios.get('/products')
+    setProduct(response?.data?.data)
+    // setProduct(response?.data?.data)
+  } 
+  useEffect(() => {
+    loadProduct()
+  }, [])
+
+
+
   return (
     <>
-    <div>
-      <h1>E-commerce Product</h1>
-    </div>
+      <div>
+        <h1 className='title'>E-commerce Product</h1>
+      </div>
 
-   
+      <div>
+        {
+          product.map((productInfo) => {
+            const { name, description, highQualtiy, price, productImage } = productInfo;
+
+            return <div className='product-card'>
+
+              <div className='container-product'>
+                <div>
+                  <img src={productImage} className='img-product' />
+                </div>
+
+                <div>
+                  <h3>Name: {name}</h3>
+                  <p>description: {description}</p>
+                  <p><b>highQuality:</b> {highQualtiy}</p>
+                  <p><b>price:</b> {price}</p>
+                </div>
+
+              </div>
+            </div>
+          })
+
+        }
+      </div>
     </>
   )
 }
